2b534aa1c3237a4259de08ede16eff0ed1d04234
Charlie Furniss <charliefurniss96@gmail.com>
2016-06-06 14:57:25 +0100

    creating array containing click rates

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -7,9 +7,43 @@ class EmailsController < ApplicationController
 
   	@emails_clicked = @emails.where(:Event => "click").count
 
-  	user_confirmation = @emails.where(:EmailType => "UserConfirmation")
+  	def create_email_type_array
 
-  	@user_confirmation_click_rate = (user_confirmation.where(:Event => "click").count * 100)
+  		email_type_array = []
+
+  		@emails.each do |email|
+
+  			email_type_array.push(email.EmailType)
+
+  		end
+
+  		return email_type_array
+
+  	end
+
+  	def calc_click_rate()
+
+  		click_rate_array = []
+
+  		email_type_array = create_email_type_array()
+
+  		email_type_array.each do |email_type|
+
+  			email = @emails.where(:EmailType => email_type)
+
+  			puts email
+
+  			click_rate = (email.where(:Event => "click").count * 100)
+
+  			click_rate_array.push(click_rate)
+
+  		end
+
+  		puts click_rate_array
+
+  	end
+
+  	calc_click_rate()
 
   end
 end
