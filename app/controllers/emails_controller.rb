5265e01d5fb7f2d6f0a100848f6bf7fc716b2465
Charlie Furniss <charliefurniss96@gmail.com>
2016-06-06 14:19:09 +0100

    basic data appearing on view

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,15 @@
 class EmailsController < ApplicationController
   def index
+
   	@emails = Email.all
+
+  	@emails_opened = (@emails.where(:Event => "open") | @emails.where(:Event => "click")).count
+
+  	@emails_clicked = @emails.where(:Event => "click").count
+
+  	user_confirmation = @emails.where(:EmailType => "UserConfirmation")
+
+  	@user_confirmation_click_rate = (user_confirmation.where(:Event => "click").count * 100)
+
   end
 end
